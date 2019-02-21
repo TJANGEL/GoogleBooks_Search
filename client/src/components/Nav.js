402f658f0be9f7cc5e3dd1f8f9a331c3e1e8256c
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-02-21 16:14:22 -0500

    updated components

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -11,19 +11,6 @@ function Nav() {
       </Link>
       <div>
         <ul className="navbar-nav">
-          {/* <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" ||
-                window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </Link>
-          </li> */}
           <li className="nav-item">
             <Link
               to="/saved"
@@ -52,6 +39,38 @@ function Nav() {
       </div>
     </nav>
   );
+}
+
+export default Nav;
+
+
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Nav() {
+    return(
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Google Books Search
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/saved"
+              className={
+                window.location.pathname === "/saved"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Saved Books
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    );
 }
 
 export default Nav;
