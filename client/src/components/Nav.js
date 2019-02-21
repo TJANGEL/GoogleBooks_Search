d5a09a077730430c07822ace44febc25206dab33
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-02-21 16:20:48 -0500

    updated components

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Nav() {
+  // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -23,54 +22,10 @@ function Nav() {
               Saved Books
             </Link>
           </li>
-          {/* <li className="nav-item">
-            <Link
-              to="/search"
-              className={
-                window.location.pathname === "/search"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Search
-            </Link>
-          </li> */}
         </ul>
       </div>
     </nav>
   );
-}
-
-export default Nav;
-
-
-import React from "react";
-import {Link} from "react-router-dom";
-
-function Nav() {
-    return(
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Google Books Search
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/saved"
-              className={
-                window.location.pathname === "/saved"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Saved Books
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    );
 }
 
 export default Nav;
